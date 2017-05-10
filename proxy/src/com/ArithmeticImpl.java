defc9333235e41e7a86586d4480f16f14c62fb5b
zhaokl <1179376017@qq.com>
2017-05-10 12:24:43 +0800

    update

---
+++ proxy/src/com/ArithmeticImpl.java
@@ -0,0 +1,18 @@
+package com;
+
+public class ArithmeticImpl implements Arithmetic {
+
+	@Override
+	public int add(int a, int b) {
+		int i=a+b;
+		return i;
+	}
+
+	aaa
+	@Override
+	public double div(int a, int b) {
+		double i=(double)a/(double)b;
+		return i;
+	}
+
+}
